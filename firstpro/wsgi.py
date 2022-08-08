4b1168496f71b443fa745404cebf1bd4873ad2db
Antarip Chatterjee <chatterjeeantarip1234@gmail.com>
2022-08-08 10:20:24 +0530

    First commit

---
+++ firstpro/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for firstpro project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firstpro.settings')
+
+application = get_wsgi_application()
